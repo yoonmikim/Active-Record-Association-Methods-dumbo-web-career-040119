648975da17d1af147f35ac88b0f53de0e7f0a6fa
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-12 14:54:43 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,6 +8,6 @@ class Song < ActiveRecord::Base
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
-    self.assign_attributes(id: Artist.find_by(name: “Drake”).id, name: "Drake")
+    # self.assign_attributes(id: Artist.find_by(name: “Drake”).id, name: "Drake")
   end
 end
